680a47c81b54c5c8ec54c7a151e8aaacc3043c4f
TanveerShahriar <tanveershahriara@gmail.com>
2023-06-18 19:48:14 +0600

    x-sum done

--- Week 3/Day 3/D_x_sum.cpp
+++ Week 3/Day 3/D_x_sum.cpp
@@ -14,6 +14,49 @@ int main(){
 
         for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) cin>>arr[i][j];
 
+        int mx = 0;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                int res = 0;
+                int ci = i, cj = j;
+                while(ci >= 0 && ci < n && cj >= 0 && cj < m)
+                {
+                    res += arr[ci][cj];
+                    ci--;
+                    cj--;
+                }
+
+                ci = i, cj = j;
+                while(ci >= 0 && ci < n && cj >= 0 && cj < m)
+                {
+                    res += arr[ci][cj];
+                    ci++;
+                    cj--;
+                }
+
+                ci = i, cj = j;
+                while(ci >= 0 && ci < n && cj >= 0 && cj < m)
+                {
+                    res += arr[ci][cj];
+                    ci--;
+                    cj++;
+                }
+
+                ci = i, cj = j;
+                while(ci >= 0 && ci < n && cj >= 0 && cj < m)
+                {
+                    res += arr[ci][cj];
+                    ci++;
+                    cj++;
+                }
+
+                res -= arr[i][j] * 3;
+                mx = max(mx, res);
+            }
+        }
+        cout<<mx<<"\n";
         
     }
 
