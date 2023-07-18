38c323a7457ea5e5cf356e50dc2f3b6fec967ca5
TanveerShahriar <tanveershahriara@gmail.com>
2023-07-18 14:53:43 +0600

    four ticket done

---
+++ Week 6/Day 6/four_ticket.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        int n;
+        cin>>n;
+
+        if(n <= 250) cout<<"YES";
+        else cout<<"NO";
+        cout<<"\n";
+    }
+
+    return 0;
+}
